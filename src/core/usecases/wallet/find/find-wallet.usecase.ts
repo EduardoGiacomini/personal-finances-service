1b7c2956fb7192d63d9edcd60b14bf340d4c7d7a
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2021-09-21 22:57:47 -0400

    feat: created the find wallet use case

--- src/core/usecases/wallet/find/find-wallet.usecase.ts
+++ src/core/usecases/wallet/find/find-wallet.usecase.ts
@@ -1,3 +1,25 @@
+import { NotFoundBusinessException } from '../../../exceptions/not-found.business.exception';
+import { WalletsRepository } from '../../../data-providers/wallets.repository';
+import { Wallet } from '../../../entities/wallet';
+
 export class FindWalletUseCase {
-  execute() {}
+  constructor(private readonly walletsRepository: WalletsRepository) {}
+
+  async execute(userId: string): Promise<Wallet> {
+    const wallet = await this.findWallet(userId);
+    this.checkIfWalletExists(wallet, userId);
+    return wallet;
+  }
+
+  async findWallet(userId: string): Promise<Wallet> {
+    return this.walletsRepository.findWalletByUserId(userId);
+  }
+
+  checkIfWalletExists(wallet: Wallet, userId: string): void {
+    if (!wallet) {
+      throw new NotFoundBusinessException(
+        `The wallet from user ${userId} does not exist`,
+      );
+    }
+  }
 }
