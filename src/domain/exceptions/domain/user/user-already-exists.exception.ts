7fd2dad9fc4751749fd0016f6bb135a018089236
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2022-03-27 22:13:26 -0400

    fix: fixed the status code

--- src/domain/exceptions/domain/user/user-already-exists.exception.ts
+++ src/domain/exceptions/domain/user/user-already-exists.exception.ts
@@ -2,7 +2,7 @@ import { Exception } from "@domain/protocol";
 
 export class UserAlreadyExistsException extends Error implements Exception {
   code = "USER_ALREADY_EXISTS";
-  status = 500;
+  status = 400;
 
   constructor() {
     super("The user already exists");
